2be174f4b404039e2df4575f0ecbd0b543ce5be5
IJJU16DEVI <devisamatham16@gmail.com>
2026-07-07 16:17:49 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,124 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print(8+9)
+17
+a=4
+b=8
+print(a+b)
+12
+c=40
+print(c)
+40
+Z=30
+print(z)
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    print(z)
+NameError: name 'z' is not defined. Did you mean: 'Z'?
+-=10
+SyntaxError: invalid syntax
+_=10
+print(_)
+10
+x=50
+print(x)
+50
+name="saii"
+print(name)
+saii
+city="kaikalur"
+print(city)
+kaikalur
+country="india"
+print(country)
+india
+2=30
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+a2=30
+print(a2)
+30
+4b=67
+SyntaxError: invalid decimal literal
+a09876=10
+print(a09876)
+10
+@=10
+SyntaxError: invalid syntax
+$=20
+SyntaxError: invalid syntax
+_=30
+print(_)
+30
+_e=40
+print(_e)
+40
+  f=67
+  
+SyntaxError: unexpected indent
+ifif=60
+if
+SyntaxError: invalid syntax
+first name="pallavi"
+SyntaxError: invalid syntax
+first_name="devi"
+print(first_name)
+devi
+a=10,b=20
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=20
+print(a,b)
+4 20
+a,b=5,6
+print(a,b)
+5 6
+a=2,3,4,55,56,7,
+print(a)
+(2, 3, 4, 55, 56, 7)
+a,b,c=6,7,8
+print(a,b,c)
+6 7 8
+>>> a,b,c=9,7,654,6,7
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    a,b,c=9,7,654,6,7
+ValueError: too many values to unpack (expected 3, got 5)
+>>> a,b,c=(1,2,3)
+>>> print(a,b,c)
+1 2 3
+>>> x,y,z=(10,20,30)
+>>> print(x,y,z)
+10 20 30
+>>> a=10
+>>> print(a)
+10
+>>> del a
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined. Did you mean: 'a2'?
+>>> fname="amma"
+>>> lname="nanna"
+>>> print(fname+lname)
+ammananna
+>>> fname="amma"
+>>> lname="nanna"
+>>> print(fname+""+lname)
+ammananna
+>>> fname="sai"
+>>> lname="devi"
+>>> print(fname+" " +lname)
+sai devi
+>>> fname="amma"
+>>> lname="nanna"
+>>> print(fname,lname)
+amma nanna
+>>> name="saikumar"
+>>> print("NAME")
+NAME
+>>> NAME="saikumar"
+>>> print("name")
+name
+>>> NAME="SAI"
+>>> print("name")
+name
